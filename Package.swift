cf3a43df44304362524a7f60f03cc33525f94437
Marcel Salej <marcelsalej@Marcels-MacBook-Pro-3.fritz.box>
2024-06-14 16:29:30 +0200

    Updated readme file for iOS
    fix
    
    XCFramevork commit
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix yml
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- Package.swift
+++ Package.swift
@@ -1,18 +1,25 @@
 // swift-tools-version: 5.6
 import PackageDescription
 
+// Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
+let name = "cryptoKMP"
+let tag = v0.0.2
+let checksum = ifjaoadfjdojfodijfodjfodjdfopfjewpfjqp
+let url = "https://github.com/globalid/crypto-kmp/archive/refs/tags/\(tag).zip"
+
+
 let package = Package(
-  name: "cryptoKMP",
+  name: name,
   platforms: [.iOS(.v14)],
   products: [
     .library(
-      name: "cryptoKMP",
-      targets: ["cryptoKMP"]
+      name: name targets: [name]
     ),
   ],
   dependencies: [],
   targets: [
-    .binaryTarget(name: "cryptoKMP",
-            path: "./distribution/cryptokmplib.xcframework"),
+   .binaryTarget(name: name,
+                 url: url,
+                 checksum: checksum),
   ]
 )
